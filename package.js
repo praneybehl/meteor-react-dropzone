2325c292e5f9ba1ddfd5fad5d450d94e5397b3ff
Praney Behl <praneybehl@gmail.com>
2015-11-05 20:49:56 +1100

    package dependencies

--- package.js
+++ package.js
@@ -1,24 +1,23 @@
 Package.describe({
   name: 'praneybehl:react-dropzone',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '3,0.0',
+  summary: 'Simple HTML5 drag-drop zone for ReactJs packaged for Meteor',
+  git: 'https://github.com/praneybehl/meteor-react-dropzone',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('react-dropzone.js');
+  api.use('react@0.1.13');
+  api.use('cosmos:browserify@0.8.3', 'client');
+  api.addFiles('package.browserify.js', 'client');
+  api.addFiles('package.browserify.options.json', 'client');
+  api.export('Dropzone');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('praneybehl:react-dropzone');
-  api.addFiles('react-dropzone-tests.js');
+
+Npm.depends({
+  "react-dropzone": "3.0.0",
+  "exposify": "0.5.0"
 });
